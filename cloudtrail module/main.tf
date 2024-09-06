3b25f5ee0a9e39eae86baccb6e9e155f3bff1823
“Parmeshwaran647” <Parameshwaran.kaliannan@bootlabstech.com>
2024-09-06 10:29:38 +0530

    cloudtrail module added

---
+++ cloudtrail module/main.tf
@@ -0,0 +1,59 @@
+resource "aws_cloudtrail" "default" {
+  name                          = var.name
+  enable_logging                = var.enable_logging
+  s3_bucket_name                = var.s3_bucket_name
+  enable_log_file_validation    = var.enable_log_file_validation
+  sns_topic_name                = var.sns_topic_name
+  is_multi_region_trail         = var.is_multi_region_trail
+  include_global_service_events = var.include_global_service_events
+  cloud_watch_logs_role_arn     = var.cloud_watch_logs_role_arn
+  cloud_watch_logs_group_arn    = var.cloud_watch_logs_group_arn
+  tags                          = .tags
+  kms_key_id                    = var.kms_key_arn
+  is_organization_trail         = var.is_organization_trail
+  s3_key_prefix                 = var.s3_key_prefix
+
+  dynamic "insight_selector" {
+    for_each = var.insight_selector
+    content {
+      insight_type = insight_selector.value.insight_type
+    }
+  }
+
+  dynamic "event_selector" {
+    for_each = var.event_selector
+    content {
+      include_management_events        = lookup(event_selector.value, "include_management_events", null)
+      read_write_type                  = lookup(event_selector.value, "read_write_type", null)
+      exclude_management_event_sources = event_selector.value.exclude_management_event_sources
+
+      dynamic "data_resource" {
+        for_each = lookup(event_selector.value, "data_resource", [])
+        content {
+          type   = data_resource.value.type
+          values = data_resource.value.values
+        }
+      }
+    }
+  }
+
+  dynamic "advanced_event_selector" {
+    for_each = var.advanced_event_selector
+    content {
+      name = lookup(advanced_event_selector.value, "name", null)
+
+      dynamic "field_selector" {
+        for_each = advanced_event_selector.value.field_selector
+        content {
+          field           = field_selector.value.field
+          equals          = lookup(field_selector.value, "equals", null)
+          not_equals      = lookup(field_selector.value, "not_equals", null)
+          starts_with     = lookup(field_selector.value, "starts_with", null)
+          not_starts_with = lookup(field_selector.value, "not_starts_with", null)
+          ends_with       = lookup(field_selector.value, "ends_with", null)
+          not_ends_with   = lookup(field_selector.value, "not_ends_with", null)
+        }
+      }
+    }
+  }
+}
